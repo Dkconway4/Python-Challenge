7e35092546bb2721d6f60c382b2a04d6d52c9edb
dkconway4 <denzellkconway@Denzells-MBP.attlocal.net>
2023-03-23 18:11:06 -0400

    Initial commit for Python Challenge

---
+++ PyPoll/main.py
@@ -0,0 +1,34 @@
+import csv,os
+election_filepath = os.path.join("Resources","election_data.csv")
+#Variables to track
+total_votes = 0
+candidate_list = 0
+voting_percentage = 0
+candidate_votes = 0
+charles_votes = 0
+diana_votes = 0
+raymon_votes = 0
+winner = None
+
+#Code to read file
+with open(election_filepath) as dataset:
+    reader = csv.reader(dataset)
+    header = next(reader)
+    thisdict = {}
+    for row in reader:
+        total_votes +=1
+        current_name = row[2]
+        if current_name in thisdict:
+            thisdict[current_name]+=1
+        else:
+            thisdict[current_name]=1
+
+for name,number in thisdict.items():
+    if not winner:
+        winner = name
+    else:
+        if number > thisdict[winner]:
+            winner = name
+    print("{}: {:.3f}% ({})".format(name,number/total_votes*100,number))
+#print(thisdict)
+print(winner)
---
+++ Pybank/main.py
@@ -0,0 +1,58 @@
+import csv,os
+
+# File path to budget data
+#budget_filepath = os.path.join("Resources","budget_data.csv")
+budget_filepath = "Resources/budget_data.csv"
+# Variables to track
+total_number_of_months = 0
+net_total = 0
+#total_profit_change = 0
+previous_total = 0
+deltas = 0
+test = list()
+running_total = 0
+avg_per_month = 0
+high_swing = 0
+low_swing = 0
+high_month = ""
+low_month = ""
+
+# Code to read file
+with open(budget_filepath) as dataset:
+    reader = csv.reader(dataset)
+    header = next(reader)
+    for row in reader:
+        total_number_of_months += 1
+        test.append((row[0],int(row[1]))) 
+        net_total += int(row[1])
+       
+       
+       # previous_total = 0 if total_number_of_months == 1 else ( total_number_of_months -1)
+    for i in range(len(test)-1):  
+
+     #total_profit_change += abs(int(row[1]) - previous_total)
+        change = test[i+1][1] - test[i][1]
+        #high_swing = max(high_swing,change)
+        #low_swing = min(low_swing, change)
+        deltas += change
+        if high_swing < change:
+            high_swing = change
+            high_month = test[i+1][0]
+           # continue
+       # else:
+           # change > high_swing
+           # high_swing = change
+
+        if low_swing > change:
+            low_swing = change
+            low_month = test[i+1][0]
+            
+
+    #for row in range(len(net_total)-1):
+        previous_total = int(row[1])
+    
+print(total_number_of_months)
+print(net_total)
+print(deltas/(len(test)-1))
+print("{} {}".format(high_month, high_swing))
+print("{} {}".format(low_month, low_swing))
